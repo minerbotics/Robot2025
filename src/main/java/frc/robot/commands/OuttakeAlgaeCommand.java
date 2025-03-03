906ea688ab6d4e6b99b5448dff1dd2e905bb1c11
koolguy906 <koolguy906@gmail.com>
2025-03-03 18:59:08 -0500

    Made the empty Algae commands work

--- src/main/java/frc/robot/commands/OuttakeAlgaeCommand.java
+++ src/main/java/frc/robot/commands/OuttakeAlgaeCommand.java
@@ -1,5 +1,26 @@
 package frc.robot.commands;
 
-public class ExtendAlgaeEffectorCommand {
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.AlgaeEffector;
 
+public class OuttakeAlgaeCommand extends Command {
+
+    private final AlgaeEffector m_algaeEffector;
+
+    public OuttakeAlgaeCommand(AlgaeEffector algaeEffector) {
+        m_algaeEffector = algaeEffector;
+        addRequirements(algaeEffector);
+    }
+
+    public void execute() {
+        m_algaeEffector.algaeOut();
+    }
+
+    public boolean isFinished() {
+        return false;
+    }
+
+    public void end(boolean interrupted) {
+        m_algaeEffector.algaeStop();
+    }
 }
--- src/main/java/frc/robot/commands/ToggleAlgaeEffectorCommand.java
+++ src/main/java/frc/robot/commands/ToggleAlgaeEffectorCommand.java
@@ -1,5 +1,18 @@
 package frc.robot.commands;
 
-public class ExtendAlgaeEffectorCommand {
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.AlgaeEffector;
 
+public class ToggleAlgaeEffectorCommand extends Command {
+
+    private final AlgaeEffector m_algaeEffector;
+
+    public ToggleAlgaeEffectorCommand(AlgaeEffector algaeEffector) {
+        m_algaeEffector = algaeEffector;
+        addRequirements(algaeEffector);
+    }
+
+    public void execute() {
+        m_algaeEffector.armsOut();
+    }
 }
